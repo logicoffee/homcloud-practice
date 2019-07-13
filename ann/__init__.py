2a2febacf3ab3def8330c61060cde17bd20418bf
Ryo Iketani <38097603+logicoffee@users.noreply.github.com>
2019-07-13 20:08:43 +0900

    Approximation (#2)
    * tmp
    
    * Fix
    
    * Implement approximate
    
    * Make a module
    
    * Organize files

---
+++ ann/__init__.py
@@ -0,0 +1,3 @@
+from .quick_select import *
+from .kd_tree import *
+from .radius_search import *
--- ann/kd_tree.py
+++ ann/kd_tree.py
@@ -1,5 +1,5 @@
 from numpy import concatenate
-from quick_select import quick_select
+from .quick_select import quick_select
 
 
 class Node:
